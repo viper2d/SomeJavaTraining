7b47ae9acc8142c9c815b9d77ad53ae1196c60c4
Vladislavs Korkuss <vladislavs.korkuss@tele2.com>
2016-07-26 11:26:25 +0300

    Initial commit

---
+++ src/MaxOfThree.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class MaxOfThree {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first num:");
+        int firstNum = sc.nextInt();
+        System.out.println("Enter second num:");
+        int secondNum = sc.nextInt();
+        System.out.println("Enter third num:");
+        int thirdNum = sc.nextInt();
+
+        if (firstNum >= secondNum && firstNum >= thirdNum){
+            System.out.println(firstNum);
+        } else if (secondNum >= firstNum && secondNum >= thirdNum){
+            System.out.println(secondNum);
+        } else {
+            System.out.println(thirdNum);
+        }
+
+    }
+}
